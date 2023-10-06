ae47e0c0ee9c107a48ff11fc646b654ed0f48e63
cscawley <cscawley@mac.com>
2023-10-05 23:55:36 -0500

    fix last warnings

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import { SectionProps } from '../../utils/SectionProps';
 import ButtonGroup from '../elements/ButtonGroup';
@@ -25,7 +25,7 @@ const Hero = ({
   ...props
 }) => {
 
-  const [videoModalActive, setVideomodalactive] = useState(false);
+  // const [videoModalActive, setVideomodalactive] = useState(false);
 
   // const openModal = (e) => {
   //   e.preventDefault();
