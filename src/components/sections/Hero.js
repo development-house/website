f9c9b8b99cb0c33a873f637e669c60b44484629c
cscawley <cscawley@mac.com>
2024-06-30 18:51:45 -0500

    update hero

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
 import { SectionProps } from '../../utils/SectionProps';
-import ButtonGroup from '../elements/ButtonGroup';
-import Button from '../elements/Button';
 import Title from '../layout/partials/Title';
 // import Image from '../elements/Image';
 // import Modal from '../elements/Modal';
